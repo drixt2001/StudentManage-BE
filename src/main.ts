553c4e19b7c172356da6b06cca2e8143ab1f0903
Thang Le Dinh <thangdrit@gmail.com>
2023-07-25 15:22:15 +0700

    [fix] update domain

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 export const host = 'http://localhost';
 export const port = process.env.PORT;
-export const domain = process.env.DOMAIN || host + ':' + port;
+const production = process.env.PRODUCTION.toLowerCase() === 'true';
+export const domain = production
+  ? process.env.DOMAIN
+  : process.env.DOMAIN || host + ':' + port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
